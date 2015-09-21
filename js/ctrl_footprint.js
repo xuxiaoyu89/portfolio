dc7722a9d7e025e656606a45a753fbdbb0b00b1a
xiaoyu xu <xuxiaoyu@xiaoyus-MacBook-Pro.local>
2015-09-20 21:11:33 -0400

    add other directives

--- js/ctrl_footprint.js
+++ js/ctrl_footprint.js
@@ -1,5 +1,7 @@
 portfolioControllers.controller("FootprintCtrl",
 	function($scope, $log){
 		$scope.msg = "hello, welcome to my world!";
+		var target = document.getElementById('page-body');
+		target.className = "footprint";
 	}
 );
