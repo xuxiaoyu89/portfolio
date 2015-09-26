989436a2d9062807fc6721649b91d1f84a3f3344
xiaoyu xu <xuxiaoyu@xiaoyus-MacBook-Pro.local>
2015-09-26 09:38:20 -0400

    add projects

--- js/ctrl_projects.js
+++ js/ctrl_projects.js
@@ -1,7 +1,25 @@
 portfolioControllers.controller("ProjectsCtrl",
 	function($scope, $log){
+
+		var showDiscription = function(event){
+			var currElement = event.srcElement;
+			var discriptions = currElement.parentNode.getElementsByClassName("project_discription");
+			discriptions[0].style.display = "block";
+		}
+
+		var hideDiscription = function(event){
+			var currElement = event.srcElement;
+			var discriptions = currElement.parentNode.getElementsByClassName("project_discription");
+			discriptions[0].style.display = "none";
+		}
+
 		$scope.msg = "hello, these are my projects.";
 		var target = document.getElementById('page-body');
 		target.className = "projects";
+		var projects = document.getElementsByClassName('project');
+		for (var i=0; i<projects.length; i++){
+			projects[i].addEventListener("mouseover", showDiscription);
+			projects[i].addEventListener("mouseout", hideDiscription);
+		}
 	}
 );
